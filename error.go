8d3bd7cade2a3ebd73253c1ae023e2e72ea9aa35
Yun <yun@yun.ink>
2024-07-31 16:47:23 +0800

    修改错误接口的方法

--- error.go
+++ error.go
@@ -5,12 +5,12 @@ import (
 )
 
 type LangError interface {
-	NewError(ctx context.Context, key string) error
-	NewErrorFormat(ctx context.Context, key string, format map[string]string) error
-	Error() string
-	GetCode() int
-	GetMsg() string
-	GetFormat() map[string]string
+	NewError(ctx context.Context, key string) error                                 // New错误
+	NewErrorFormat(ctx context.Context, key string, format map[string]string) error // New错误
+	Error() string                                                                  // 实现error接口&获取翻译后的错误信息
+	GetCode() int                                                                   // 获取翻译后的Code
+	GetKey() string                                                                 // 获取原Key值
+	GetFormat() map[string]string                                                   // 获取附加数据
 }
 
 type langError struct {
@@ -20,8 +20,6 @@ type langError struct {
 }
 
 type errorConst string
-
-const errorLang errorConst = "errorLang"
 
 func (e *langError) Error() string {
 	errLang := e.ctx.Value(errorLang)
@@ -36,7 +34,7 @@ func (e *langError) GetCode() int {
 	return GetCode(e.key)
 }
 
-func (e *langError) GetMsg() string {
+func (e *langError) GetKey() string {
 	return e.key
 }
 
@@ -61,6 +59,8 @@ func NewError(ctx context.Context, key string) error {
 		key: key,
 	}
 }
+
+const errorLang errorConst = "errorLang"
 
 func SetCtxLang(ctx context.Context, lang string) context.Context {
 	return context.WithValue(ctx, errorLang, lang)
